61123a43a2ac4d285711dd28e618b4ee6dd21f43
Kirby <kafracas@gmail.com>
2020-12-17 19:49:14 -0600

    completed studio

---
+++ Studio_Counting-Characters/Studio_Counting-Characters/CharacterCounter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studio_Counting_Characters
+{
+    class CharacterCounter
+    {
+        public static void Counter()
+        {
+            //string sampleString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            Console.WriteLine("Enter a string: ");
+            string stringInput = Console.ReadLine();
+            char[] charactersInString = stringInput.ToCharArray();
+            Dictionary<string, int> characterCounts = new Dictionary<string, int>();
+            foreach (char i in charactersInString)
+            {
+                if (Char.IsLetter(i))
+                {
+                    string thisChar = Char.ToUpper(i) + "/" + Char.ToLower(i);
+                    int thisCharCount = 0;
+                    foreach (char c in charactersInString)
+                    {
+                        if (Char.ToLower(c) == Char.ToLower(i))
+                        {
+                            thisCharCount += 1;
+                        }
+                    }
+                    if (!characterCounts.ContainsKey(thisChar))
+                    {
+                        characterCounts.Add(thisChar, thisCharCount);
+                    }
+                }
+            }
+            foreach (KeyValuePair<string, int> printChar in characterCounts)
+            {
+                Console.WriteLine(printChar.Key + ": " + printChar.Value);
+            }
+        }
+
+    }
+}
---
+++ Studio_Counting-Characters/Studio_Counting-Characters/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Studio_Counting_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CharacterCounter.Counter();
+        }
+    }
+}
